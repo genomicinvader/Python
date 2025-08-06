e62e8617d6dff1c336fcb4eacf785de9b5b0e0e7
genomicinvader <anikangel664@gmail.com>
2025-08-06 13:11:16 +0600

    Fix input prompt formatting in character counter program

--- count_name_character.py
+++ count_name_character.py
@@ -1,6 +1,6 @@
 print("Welcome to the character counter program!\nThis program will count the number of characters in your name.")
 
-name = input("Please enter your name: \n")
+name = input("Please enter your name:\n")
 
 count = len(name)
 
